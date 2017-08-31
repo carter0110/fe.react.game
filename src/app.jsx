e40aef57307c61a8eb5f3b9a4cd43482231171a8
Carter <han730110@gmai.com>
2017-09-01 00:48:04 +0800

    First commit.

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+require('index.html');
+require('style.scss');
+
+var GridBox = require('GridBox');
+
+// var data = [
+//   {id:1, author:'某甲', text:'我有一支筆'},
+//   {id:2, author:'某乙', text:'我有一顆蘋果'},
+// ];
+
+//var url = 'https://comment-server-scars377.c9users.io/';
+
+ReactDOM.render(
+  <GridBox/>,
+  document.getElementById('content')
+);
---
+++ src/components/GridBox.jsx
@@ -0,0 +1,14 @@
+var GridBox = React.createClass({
+    getInitialState: function(){
+        return {
+            singleGrid: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        };
+    },
+    render: function(){
+        return (
+            <div>111</div>
+        )
+    }
+});
+
+module.exports = GridBox;
